e1031fcee827b30884a5a3658c216ae2c81fa296
Ryu Tae Hyun <ryu200112@gmail.com>
2026-08-31 21:35:37 +0900

    Update SIDEY cask to alpha.6

--- Casks/sidey.rb
+++ Casks/sidey.rb
@@ -1,6 +1,6 @@
 cask "sidey" do
-  version "0.2.0-alpha.5"
-  sha256 "caee1e5b09a5e06758583c8d0ad87f789eded51e1719eebaad91169985c0c36b"
+  version "0.2.0-alpha.6"
+  sha256 "9305980a0efcfb0a23efb2071b42336adffc40c1817826cbf6520271159abbe7"
 
   url "https://github.com/sidey-app/SIDEY/releases/download/v#{version}/SIDEY-macOS-arm64-v#{version}.dmg"
   name "SIDEY"
