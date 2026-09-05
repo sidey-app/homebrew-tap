acbed1891dc43c98ca62817632b0107bfd33eb22
Ryu Tae Hyun <ryu200112@gmail.com>
2026-09-05 19:39:08 +0900

    Update SIDEY to 1.0.8

--- Casks/sidey.rb
+++ Casks/sidey.rb
@@ -1,6 +1,6 @@
 cask "sidey" do
-  version "1.0.7"
-  sha256 "59e109c56b873c67502e0393b8863cdea215894136375960fd149a112cbf9890"
+  version "1.0.8"
+  sha256 "f268621890a57ceaaf247675e354fa0dcef60ecca567ff65360d49b68100d66b"
 
   url "https://github.com/sidey-app/SIDEY/releases/download/v#{version}/SIDEY-macOS-arm64-v#{version}.dmg"
   name "SIDEY"
